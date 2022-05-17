cf01cc608f2d6d3bc5a9f0912473fc1b670a8e66
Misael Gomes Macedo <102962890+Homi-Das-Carsa-Mole@users.noreply.github.com>
2022-05-17 15:12:54 -0300

    Update Main.java

--- FundamentosJavinha/src/Main.java
+++ FundamentosJavinha/src/Main.java
@@ -13,8 +13,9 @@ public class Main {
 		short z = (int) 10000;
 		System.out.println(z);
 		
-		a = (int) y; //Casting Explícito: é necessário deixar explícito que se deseja passar o conteúdo da variável para outro tipo que não a suporta completamente (assume-se a responsabilidade da perca de dados).
-		b = x; //Casting Implícito: é possível passar o conteúdo de uma variável para outra.
+		a = (int) y; //Casting ExplÃ­cito: Ã© necessÃ¡rio deixar explÃ­cito que se deseja passar o conteÃºdo da variÃ¡vel para outro tipo que nÃ£o a suporta
+		//completamente (assume-se a responsabilidade da perca de dados).
+		b = x; //Casting ImplÃ­cito: Ã© possÃ­vel passar o conteÃºdo de uma variÃ¡vel para outra.
 		
 		System.out.println(x);
 		System.out.println(y);
@@ -23,56 +24,56 @@ public class Main {
 		
 		int idadePessoa = 70;
 		if (idadePessoa < 18) {
-			System.out.println("Você não pode beber.");
+			System.out.println("VocÃª nÃ£o pode beber.");
 		}
 		else if (idadePessoa < 60) {
-			System.out.println("Você pode beber à vontade, meu camaradis da cachacis.");
+			System.out.println("VocÃª pode beber Ã  vontade, meu camaradis da cachacis.");
 		}
 		else {
 			System.out.println("Volta pro asilo.");
 		}
 		
-			System.out.println("Você tem " + idadePessoa + " anos.");
+			System.out.println("VocÃª tem " + idadePessoa + " anos.");
 		*/
 			
 //			int num = 1;
 //			
 //			switch (num) {
 //				case 1:
-//					System.out.println("O seu número é " + num + ".");
+//					System.out.println("O seu nÃºmero Ã© " + num + ".");
 //					break;
 //				case 2:
-//					System.out.println("O seu número é " + num + ".");
+//					System.out.println("O seu nÃºmero Ã© " + num + ".");
 //					break;
 //				case 3:
-//					System.out.println("O seu número é " + num + ".");
+//					System.out.println("O seu nÃºmero Ã© " + num + ".");
 //					break;
 //				case 4:
-//					System.out.println("O seu número é " + num + ".");
+//					System.out.println("O seu nÃºmero Ã© " + num + ".");
 //					break;
 //				case 5:
-//					System.out.println("O seu número é " + num + ".");
+//					System.out.println("O seu nÃºmero Ã© " + num + ".");
 //					break;
 //				}
 //					
-//				//O break impede que o restante do código dentro do switch seja executado. Quando utilizado, ele sai do switch.
+//				//O break impede que o restante do cÃ³digo dentro do switch seja executado. Quando utilizado, ele sai do switch.
 //					
 //				int num2 = 5;
 //				
 //				switch (num) {
 //					case 1:
-//						System.out.println("O seu número é " + num2 + ".");
+//						System.out.println("O seu nÃºmero Ã© " + num2 + ".");
 //					case 2:
-//						System.out.println("O seu número é " + num2 + ".");
+//						System.out.println("O seu nÃºmero Ã© " + num2 + ".");
 //					case 3:
-//						System.out.println("O seu número é " + num2 + ".");
+//						System.out.println("O seu nÃºmero Ã© " + num2 + ".");
 //					case 4:
-//						System.out.println("O seu número é " + num2 + ".");
+//						System.out.println("O seu nÃºmero Ã© " + num2 + ".");
 //					case 5:
-//						System.out.println("O seu número é " + num2 + ".");
+//						System.out.println("O seu nÃºmero Ã© " + num2 + ".");
 //				}
 					
-				//Sem o break, ocorrerá uma repetição do código. Apesar disso, não utilizar o break também pode ser útil em alguns casos.
+				//Sem o break, ocorrerÃ¡ uma repetiÃ§Ã£o do cÃ³digo. Apesar disso, nÃ£o utilizar o break tambÃ©m pode ser Ãºtil em alguns casos.
 				
 //				int num3 = 8;
 //				switch (num3) {
@@ -83,7 +84,7 @@ public class Main {
 //						System.out.println("Segunda");
 //						break;
 //					case 3:
-//						System.out.println("Terça");
+//						System.out.println("TerÃ§a");
 //						break;
 //					case 4:
 //						System.out.println("Quarta");
@@ -95,23 +96,23 @@ public class Main {
 //						System.out.println("Sexta");
 //						break;
 //					case 7:
-//						System.out.println("Sábado");
+//						System.out.println("SÃ¡bado");
 //						break;
 //					default:
-//						System.out.println("Esse número não representa um dia da semana.");
+//						System.out.println("Esse nÃºmero nÃ£o representa um dia da semana.");
 //			}
 				
 //			char caractere = 'a';
 //			
 //			switch(caractere) {
 //				case 'a':
-//					System.out.println("O seu caractere é " + caractere + ".");
+//					System.out.println("O seu caractere Ã© " + caractere + ".");
 //					break;
 //				case 'b':
-//					System.out.println("O seu caractere é " + caractere + ".");
+//					System.out.println("O seu caractere Ã© " + caractere + ".");
 //					break;
 //				default:
-//					System.out.println("Valor inválido.");
+//					System.out.println("Valor invÃ¡lido.");
 //			}
 			
 //			String farol = "verde";
@@ -121,13 +122,13 @@ public class Main {
 //					System.out.println("SIGA.");
 //					break;
 //				case "amarelo":
-//					System.out.println("ATENÇÃO.");
+//					System.out.println("ATENÃ‡ÃƒO.");
 //					break;
 //				case "vermelho":
 //					System.out.println("PARE.");
 //					break;
 //				default:
-//					System.out.println("Valor inválido.");
+//					System.out.println("Valor invÃ¡lido.");
 //			}
 		
 //			x = 0;
@@ -135,14 +136,14 @@ public class Main {
 //				x = x + 1; // x++
 //				System.out.println(x);
 //			} 
-			//Neste caso, é primeiro feito o teste condicional. Enquanto o teste condicional for verdadeiro, o código dentro do while será executado.
+			//Neste caso, Ã© primeiro feito o teste condicional. Enquanto o teste condicional for verdadeiro, o cÃ³digo dentro do while serÃ¡ executado.
 			
 //			x = 0;
 //			do {
 //				System.out.println(x);
 //				x = x + 1;
 //			} while (x < 10);
-			//Já no "do while", é primeiro executado o código e depois feito o teste condicional. Enquanto o valor do teste condicional atender a condição estipulada, o código será  executado.
+			//JÃ¡ no "do while", Ã© primeiro executado o cÃ³digo e depois feito o teste condicional. Enquanto o valor do teste condicional atender a condiÃ§Ã£o estipulada, o cÃ³digo serÃ¡  executado.
 			
 //			for(int x=0; x <= 10; x++) { // (valor inicial; teste condicional; incremento)
 //				System.out.printf("2 x %d = %d%n", x, 2 * x);
@@ -153,10 +154,10 @@ public class Main {
 //			for(int x = 0; x <= 10; x++)
 //				System.out.println("2 x " + x + " = " + 2 * x);
 //			
-//			//Com inserção de número
+//			//Com inserÃ§Ã£o de nÃºmero
 //				
 //			Scanner entrada = new Scanner (System.in);
-//			System.out.println("Insira um número:");
+//			System.out.println("Insira um nÃºmero:");
 //			int y = entrada.nextInt();
 //			
 //			for(int x = 0; x <= 10; x++) {
@@ -170,15 +171,15 @@ public class Main {
 //			System.out.println(x1);
 //			System.out.println(y1);
 //			System.out.println(z1);
-//			System.out.println(); //Nesse caso, foi necessário criar um SYSO para a exibição do valor de cada uma das variáveis. 
+//			System.out.println(); //Nesse caso, foi necessÃ¡rio criar um SYSO para a exibiÃ§Ã£o do valor de cada uma das variÃ¡veis. 
 //			
-			double x[] = {3.5, 5, 3, 5}; //As arrays, ou matrizes, permitem que sejam atribuídos diversos valores para uma mesma variável. As arrays são construídas através do uso
+			double x[] = {3.5, 5, 3, 5}; //As arrays, ou matrizes, permitem que sejam atribuÃ­dos diversos valores para uma mesma variÃ¡vel. As arrays sÃ£o construÃ­das atravÃ©s do uso
 			//de colchetes.
 			
-			for (int aux = 0; aux < x.length; aux++) { //Criamos um laço de repetição onde o número inicial do índice, representado pela variável aux, é 0.
-//			O índice será incrementado, recebendo +1, enquanto o valor do índice (aux) for menor que o número de posições total da matriz que criamos.
-//			A cada repetição será executado o código abaixo, que exibe o valor inserido em cada posição/índice da matriz.
-				System.out.println("Valor do índice " + aux + " = " + x[aux]);
+			for (int aux = 0; aux < x.length; aux++) { //Criamos um laÃ§o de repetiÃ§Ã£o onde o nÃºmero inicial do Ã­ndice, representado pela variÃ¡vel aux, Ã© 0.
+//			O Ã­ndice serÃ¡ incrementado, recebendo +1, enquanto o valor do Ã­ndice (aux) for menor que o nÃºmero de posiÃ§Ãµes total da matriz que criamos.
+//			A cada repetiÃ§Ã£o serÃ¡ executado o cÃ³digo abaixo, que exibe o valor inserido em cada posiÃ§Ã£o/Ã­ndice da matriz.
+				System.out.println("Valor do Ã­ndice " + aux + " = " + x[aux]);
 			}
 			
 			String nome[] = {"Misael", "Neide", "Milton", "Gabriel"};
@@ -186,28 +187,28 @@ public class Main {
 			for (int aux1 = 0; aux1 < nome.length; aux1++) {
 				System.out.println(nome[aux1]);
 				if (nome[aux1] == "Gabriel") {
-					System.out.println("O índice do nome Gabriel é " + aux1);
+					System.out.println("O Ã­ndice do nome Gabriel Ã© " + aux1);
 				}
 			}
 			
-			//É possível criar matrizes vazias, sem contéudo algum. Exemplo de como fazer:
+			//Ã‰ possÃ­vel criar matrizes vazias, sem contÃ©udo algum. Exemplo de como fazer:
 			
-			int[] arrayInt = new int[5]; //Aqui, construímos uma array do tipo inteiro. Ela terá 5 posições que poderão, futuramente, ser ocupadas com valores inteiros. No entanto,
-			//no momento de criação, elas estão vazias. O mesmo se dá com as matrizes abaixo.
+			int[] arrayInt = new int[5]; //Aqui, construÃ­mos uma array do tipo inteiro. Ela terÃ¡ 5 posiÃ§Ãµes que poderÃ£o, futuramente, ser ocupadas com valores inteiros. No entanto,
+			//no momento de criaÃ§Ã£o, elas estÃ£o vazias. O mesmo se dÃ¡ com as matrizes abaixo.
 			String[] arrayString = new String[4];
 			double[] arrayDouble = new double[3];
 			
-			arrayInt[3] = 4; //Estamos atribuindo o valor 4 para a posição 3 da matriz.
+			arrayInt[3] = 4; //Estamos atribuindo o valor 4 para a posiÃ§Ã£o 3 da matriz.
 			
 			int[] numero = new int[6];
 			Scanner teclado = new Scanner(System.in);
 			
 			for (int aux2 = 0; aux2 < numero.length; aux2++) {
-				System.out.println("Digite um número qualquer");
+				System.out.println("Digite um nÃºmero qualquer");
 				numero[aux2] = teclado.nextInt();
 			}
 			for (int aux2 = 0; aux2 < numero.length; aux2++) {
-				System.out.println("O índice do número " + numero[aux2] + " é igual a " + aux2);
+				System.out.println("O Ã­ndice do nÃºmero " + numero[aux2] + " Ã© igual a " + aux2);
 			}
 			
 			

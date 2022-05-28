693a1d9658306d00a5deac0b921f8cc66fdbab5d
Misael Gomes Macedo <102962890+MisaelGitHub-jpg@users.noreply.github.com>
2022-05-28 17:34:55 -0300

    Comentários

--- Sobrecarga/src/Matematica.java
+++ Sobrecarga/src/Matematica.java
@@ -3,7 +3,7 @@ public class Matematica {
 	
 	//É possível fazer com que um mesmo método seja utilizado de diferentes formas dependendo de seus parâmetros.
 	
-	int somar(int x, int y) { //O método "somar", nesse primeiro caso, recebe em seu construtor o parâmetro de dois valores inteiros, x e y.
+	int somar(int x, int y) { //O método "somar", nesse primeiro caso, recebe em seu construtor os parâmetros de dois valores inteiros, x e y.
 		
 		//int z = x + y;
 		//return z;

2914234f978ee0f56622f2f8d9faf4b4fc493669
Misael Gomes Macedo <102962890+MisaelGitHub-jpg@users.noreply.github.com>
2022-06-02 00:10:07 -0300

    Comentários

--- ClasseAbstrata/src/Cogumelo.java
+++ ClasseAbstrata/src/Cogumelo.java
@@ -1,8 +1,8 @@
 
-public class Cogumelo extends Item {
+public class Cogumelo extends Item { //A classe Cogumelo se estende da classe Item, já que Cogumelo é um item, logo, ela possui acesso aos métodos inseridos em Item.
 
 	@Override
-	public void pegar() {
+	public void pegar() { //Método "pegar" sobrescrito por meio do @Override, especificando uma ação particular da classe Cogumelo.
 		System.out.println("Pegou Cogumelo.");
 	}
 

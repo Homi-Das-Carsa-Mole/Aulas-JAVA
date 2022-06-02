da82b168f615b566b79c8545dc993a7338df80e6
Misael Gomes Macedo <102962890+zMSLz@users.noreply.github.com>
2022-06-02 02:36:25 -0300

    Comentários

--- ExercicioInterface1/src/Circunferencia.java
+++ ExercicioInterface1/src/Circunferencia.java
@@ -1,9 +1,12 @@
 
-public class Circunferencia implements AreaCalculavel {
+public class Circunferencia implements AreaCalculavel { //Implementamos a interface AreaCalculavel na classe Circunferencia para que seja possível usufruir de seus métodos.
 
-	@Override
+	@Override //Visto que as formas geométricas selecionadas possuem formas distintas de se calcular a área, fazemos um Override do método "calcularArea",
+	//que está na interface AreaCalculavel, de modo a especificar como desejamos realizar o cálculo.
 	public double calcularArea(double x, double y) {
-		return Math.pow(x, 2) * Math.PI;
+		return Math.pow(x, 2) * Math.PI; //Utilização da classe "Math", a qual contém métodos voltados para cálculos matemáticos.
+		//Math.pow serve para realizar uma potenciação. O primeiro valor do parâmetro representa a base da potência, enquanto o segundo valor define seu expoente.
+		//Math.PI representa o número PI.
 	}
 
 }

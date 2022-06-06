099c8638330da3742cbe6104b649c389892a9103
Gabriel <Gabriel@DESKTOP-692JECS>
2022-06-06 01:39:43 -0300

    Criando o código novamente e comentando

---
+++ Polimorfismo/src/Animal.java
@@ -0,0 +1,9 @@
+
+public class Animal {
+
+	//Foi feito um Override no método abaixo em cada classe que se estende da superclasse Animal para individualizar a fala de cada animal.
+	public void Falar() {
+		System.out.println("");
+	}
+	
+}

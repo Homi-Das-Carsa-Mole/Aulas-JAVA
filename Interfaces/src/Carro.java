65eac3e7c59b472387ba7ae75e7c795ab944e4ea
Misael Gomes Macedo <102962890+zMSLz@users.noreply.github.com>
2022-06-06 19:47:19 -0300

    Comentários: interface que herda uma interface

--- Interfaces/src/Carro.java
+++ Interfaces/src/Carro.java
@@ -1,5 +1,6 @@
 
-public interface Carro extends Automovel {
+public interface Carro extends Automovel { //A fim de reaproveitar métodos em comum, uma interface pode se extender de outra, assim como, neste caso, a interface "Carro"
+	//se extende da interface "Automóvel", herdando os métodos nela inseridos. 
 	
 	public void abrirPorta();
 }

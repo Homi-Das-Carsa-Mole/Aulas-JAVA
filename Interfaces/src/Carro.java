7e87cdaf1031b07dbda7a6f6c368416ef47a2361
Misael Gomes Macedo <102962890+zMSLz@users.noreply.github.com>
2022-06-06 20:14:51 -0300

    dfdsf

--- Interfaces/src/Carro.java
+++ Interfaces/src/Carro.java
@@ -1,6 +1,6 @@
 
 public interface Carro extends Automovel { //A fim de reaproveitar métodos em comum, uma interface pode se extender de outra, assim como, neste caso, a interface "Carro"
-	//se extende da interface "Automóvel", e passa a herdar os métodos nela inseridos.
+	//se extende da interface "Automóvel", e passa a herdar os métodos nela inseridos..
 	
 	public void abrirPorta();
 }

13c44d5ff9039bc228a436832b4a94283239b7a5
Misael Gomes Macedo <102962890+zMSLz@users.noreply.github.com>
2022-06-07 09:05:53 -0300

    Comments: definindo Herança

--- Polimorfismo/src/Animal.java
+++ Polimorfismo/src/Animal.java
@@ -9,7 +9,7 @@ public class Animal { //A classe "Animal" é uma classe pai, ou superclasse, poi
 	//temos a representação de um dos pilares fundamentais de POO: a Herança.
 	
 	//Atributos e métodos genéricos são, respectivamente, características e comportamentos definidos numa superclasse que serão comuns a todas as classes filhas.
-	//Nesse caso, todo animal tem a capacidade de falar. Logo, "falar" pode ser um método genérico da classe "Animal. Ainda, todo animal pertence a uma determinada
+	//Nesse caso, todo animal tem a capacidade de falar. Logo, "falar" pode ser um método genérico da classe "Animal". Ainda, todo animal pertence a uma determinada
 	//espécie. Espécie, portanto, também pode ser um atributo genérico da classe "Animal".
 	//Obviamente, a forma como um animal se comunica dependerá de sua espécie. Cachorros e gatos, por exemplo, não produzem o mesmo tipo de "fala"; um diz "au-au" e 
 	//outro "miau". Apesar dessa diferença, nada muda o fato de que ambos estão falando algo; esse é um comportamento, ou ação, comum entre os dois.

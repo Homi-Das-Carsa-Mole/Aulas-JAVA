4167d68728630077174d4b275f23b502269a6751
Misael Gomes Macedo <102962890+zMSLz@users.noreply.github.com>
2022-06-10 22:00:06 -0300

    Comments: Override do método abstrato "pegar"

--- ClasseAbstrata/src/Cogumelo.java
+++ ClasseAbstrata/src/Cogumelo.java
@@ -1,5 +1,6 @@
 
-public class Cogumelo extends Item { //A classe Cogumelo se estende da classe Item, já que Cogumelo é um item, logo, ela possui acesso aos métodos inseridos em Item.
+public class Cogumelo extends Item { //Já que os cogumelos representam um tipo de item disponível em nosso jogo, a classe Cogumelo se estende da classe abstrata "Item",
+	//possuindo acesso aos método genérico "pegar" inserido nela. O mesmo se aplica às demais classes filhas de "Item".
 
 	@Override
 	public void pegar() { //Método "pegar" sobrescrito por meio do @Override, especificando uma ação particular da classe Cogumelo.

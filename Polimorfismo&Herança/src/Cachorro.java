4679fcd709c32ad8bbc1584d05867918f0c5830f
Misael Gomes Macedo <102962890+zMSLz@users.noreply.github.com>
2022-06-11 23:48:43 -0300

    Comments: Polimorfismo & Herança

--- Polimorfismo&Herança/src/Cachorro.java
+++ Polimorfismo&Herança/src/Cachorro.java
@@ -1,5 +1,7 @@
 
-public class Cachorro extends Animal {
+public class Cachorro extends Animal {//Dizemos que uma classe herda os atributos e métodos de outra por meio da palavra "extends". Por fazer uso de "extends", afirmamos, nesse
+	//caso, que a classe "Cachorro" se estende da classe "Animal", logo, "Cachorro" torna-se uma classe filha da classe "Animal", agora classificada como "superclasse" ou
+	//"classe pai".
 
 	@Override 
 	public void Falar() {

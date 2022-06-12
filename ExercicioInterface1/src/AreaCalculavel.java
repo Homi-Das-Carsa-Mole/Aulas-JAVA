81d5c22eefdea14d92f7911305172c73842ee894
Misael Gomes Macedo <102962890+zMSLz@users.noreply.github.com>
2022-06-12 00:00:31 -0300

    Comments:  Interface

--- ExercicioInterface1/src/AreaCalculavel.java
+++ ExercicioInterface1/src/AreaCalculavel.java
@@ -1,15 +1,16 @@
 
-public interface AreaCalculavel { 
+public interface AreaCalculavel {
 	
 	//Em uma interface, não inserimos atributos, apenas métodos.
 	//Estes métodos, porém, possuem apenas sua assinatura. Não há o desenvolvimento de qualquer tipo de código dentro dele.
 	//Uma interface serve para que diferentes classes possam se apropriar dos métodos nela implementados.
-	//Cada classe pode utilizar dos métodos de uma interface da maneira que desejar, desde que se faça um Override deste método.
+	//Cada classe pode utilizar dos métodos de uma interface da maneira que desejar, desde que se faça um Override destes métodos.
 	
-	//Neste caso, a interface AreaCalculavel possui a assinatura de um método do tipo double chamado calcularArea, 
+	//Neste caso, a interface "AreaCalculavel" possui a assinatura de um método do tipo double chamado "calcularArea", 
 	//recebendo como parâmetros duas variáveis do tipo double.
 	
-	//O método abaixo será implementado nas demais classes, a fim de que se possa calcular a área de cada forma geométrica que determinamos.
+	//O método abaixo será acessível para as demais classes que implementarem a interface "AreaCalculavel", a fim de que se possa calcular a área
+	//de cada forma geométrica que escolhemos fazer uso neste projeto.
 	
 	double calcularArea(double x, double y); 
 	

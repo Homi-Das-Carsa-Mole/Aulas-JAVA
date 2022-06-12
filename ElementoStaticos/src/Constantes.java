a02fe1142f1800408d4ed06eb9264f2c545d34b6
Misael Gomes Macedo <102962890+zMSLz@users.noreply.github.com>
2022-06-11 21:25:15 -0300

    Comments: Constantes

--- ElementoStaticos/src/Constantes.java
+++ ElementoStaticos/src/Constantes.java
@@ -1,9 +1,13 @@
 
-public class Constantes {
-//	public static final double MEDIA_DA_PROVA = 7.0;
-	public static final double MEDIA_DA_PROVA;
+public class Constantes { //Uma constante, diferente de uma variável, é um elemento que não sofre alteração de valor, ou seja, ele é um elemento estático.
 	
-	static {
-		MEDIA_DA_PROVA = 7.0;
+	public static final double MEDIA_DA_PROVA //Uma constante é declarada por meio da sentença "static final". Para nomear uma constante, se faz uso da caixa alta, isto é,
+	//todos os caracteres que compõem o nome atribuído à constante devem ser grafados com letra maiúscula. Em caso de constante de nome composto, como é o caso de "Média
+	//da Prova", as palavras são separadas por um subtraço, conhecido como "underline", resultando, então, em "MEDIA_DA_PROVA". 
+	
+	static { //Definindo o valor estático da constante "MEDIA_DA_PROVA":
+		MEDIA_DA_PROVA = 7.0; //Essa constante recebe 7.0 como seu valor estático/imutável.
 	}
+	
+	//Outra forma de criar a mesma constante acima: public static final double MEDIA_DA_PROVA = 7.0;
 }

e15aaf241a7b798dd673de60b2a7fbe19696c59c
Misael Gomes Macedo <102962890+zMSLz@users.noreply.github.com>
2022-06-12 08:54:49 -0300

    Comments

--- ExercicioContaBancaria/src/ContaInvestimento.java
+++ ExercicioContaBancaria/src/ContaInvestimento.java
@@ -1,10 +1,11 @@
 
 public class ContaInvestimento extends ContaBancaria {
-	private double saldo;
 	
 	@Override
+	//Ao calcular o saldo de uma conta do tipo "ContaIvestimento", acrescenta-se ao saldo final 5% do saldo em conta, referente aos rendimentos do
+	//dinheiro investido.
 	public double calcularSaldo() {
-		return saldo += (saldo * 0.05);
+		return saldo += (this.saldo * 0.05);
 	}
 	
 }

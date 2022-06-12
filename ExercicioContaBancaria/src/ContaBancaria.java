fdfabbbedf3b9e870055e808001a031753e6f22f
Misael Gomes Macedo <102962890+zMSLz@users.noreply.github.com>
2022-06-12 08:53:53 -0300

    Comments

--- ExercicioContaBancaria/src/ContaBancaria.java
+++ ExercicioContaBancaria/src/ContaBancaria.java
@@ -1,19 +1,34 @@
 
-public abstract class ContaBancaria {
-	protected static double saldo;
+public abstract class ContaBancaria {//Superclasse abstrata da qual "ContaCorrente" e "ContaInvestimento" se estendem, herdando seus atributos e métodos. 
+	protected double saldo; //Atributo "saldo" com modificador de acesso "protected", a fim de que somente essa superclasse e suas classes filhas
+	//consigam ver este atributo.
 	
-	public void depositar(double deposito) {
-		this.saldo += deposito;
+	//Método que permite depositar um valor, o atribuindo ao saldo da conta:
+	public void depositar(double valor) {
+		saldo += valor;
 	}
 	
-	public void sacar(double saque) {
-		this.saldo -= saque;
+	//Método para sacar dinheiro da conta:
+	public void sacar(double saque) {//Se o saldo da conta for maior ou igual ao valor do saque, o saque será realizado.
+		if (this.saldo >= saque) {
+			this.saldo -= saque;
+			
+		} else {//Se o saldo da conta for menor que o valor do saque, o saque não será realizado.
+			System.out.println("Não é possível realizar o saque, saldo insuficiente.");
+		}
 	}
 	
-	public void transferir(double transferencia, ContaBancaria conta) {
+	//Método para transferir valores de uma conta para outra:
+	public void transferir(ContaBancaria conta, double transferencia) {
+		if (this.saldo >= transferencia) {
+		this.saldo -= transferencia;
 		conta.saldo += transferencia;
+		}
 	}
 	
+	//Método abstrato, já que cada conta calcula seu saldo seguindo uma fórmula específica:
 	public abstract double calcularSaldo();
+}
+
 
 }

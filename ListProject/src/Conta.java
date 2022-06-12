3265af88c8072a53a61aaba19b7f6e62ed36119b
Misael Gomes Macedo <102962890+zMSLz@users.noreply.github.com>
2022-06-12 11:25:54 -0300

    Comments: List

--- ListProject/src/Conta.java
+++ ListProject/src/Conta.java
@@ -11,6 +11,8 @@ public class Conta {
 		this.valor = valor;
 	}
 	@Override
+	//Para que as informações de uma lista possam ser impressas no console, faz-se uso do método "toString". É possível manter a formatação de exibição original, gerada 
+	//automaticamente através do "Generate toString()", mas personalizar como as informações abaixo aparecerão na tela também é uma opção.
 	public String toString() {
 		return "Conta [nome=" + nome + ", valor=" + valor + "]";
 	}

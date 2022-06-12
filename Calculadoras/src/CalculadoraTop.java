61aecef5af4e11d479808491fd01e7aa1260b8e4
Misael Gomes Macedo <102962890+zMSLz@users.noreply.github.com>
2022-06-12 14:12:21 -0300

    Comments: CalculadoraTop

--- Calculadoras/src/CalculadoraTop.java
+++ Calculadoras/src/CalculadoraTop.java
@@ -1,51 +1,51 @@
-import java.util.Scanner; /*
-"java.util" é um pacote do Java que contém diversas utilidades como que "pré-instaladas" no próprio Java e que apenas precisam ser importadas
-para que façamos uso. A classe Scanner, a qual estamos importando para este projeto, é um exemplo de utilidade que o pacote "java.util" consegue nos oferecer.
-O Scanner, de forma resumida, é uma classe que contém métodos responsáveis por viabilizar a leitura de informações que o teclado do usuário fornece para o console. Graças
-ao Scanner, podemos, por exemplo, digitar um valor no console e atribuí-lo à uma variável no momento de execução do código.
-*/
+import java.util.Scanner;//Scanner para a leitura de dados digitados no console.
 
+//Calculadora que desenvolvemos com a professora Mayara. Conta com a função de escolha das operações desejadas.
 public class CalculadoraTop {
 
 	public static void main(String[] args) {
-		//Criação das variáveis que receberão os valores desejados pelo usuário com o objetivo de realizar um cálculo matemático:
-		int n1, n2;
 		
-		String op = ""; //Variável do tipo String que guardará qual a operação matemática o usuário gostaria de utilizar: adição (+), subtração (-), multiplicação
-		//(*) e divisão (/).
-		int control = 0; //Variável de controle que será incrementada dentro das iterações de "op"
-		
-		Scanner entrada =  new Scanner(System.in);
-		
-		System.out.println("Digite a operação desejada (+, -, *, /): ");
-		op = entrada.nextLine();
-		
-		System.out.println("Digite o primeiro valor: ");
-		n1 = Integer.parseInt(entrada.nextLine());
-		
-		System.out.println("Digite o segundo valor: " );
-		n2 = Integer.parseInt(entrada.nextLine());
-		
-		if(op.equals("+")) {
-			System.out.println("O resultado da soma é: " + (n1 + n2));
-			control++;
-		}	
-		if(op.equals("-")) {
-			System.out.println("O resultado da subtração é: " + (n1 - n2));
-			control++;
-		}
-		if(op.equals("*")) {
-			System.out.println("O resultado da multiplicação é: " + (n1 * n2));
-			control++;
-		}
-		if(op.equals("/")) {
-			System.out.println("O resultado da divisão é: " + (n1 / n2));
-			control++;
-		}
-		if(control == 0) {
-			System.out.println("Operação inválida, tente novamente.");
-		}
-		}
-		
-	}
+		//Instanciando o objeto "entrada" do tipo "Scanner", o qual armazenará os dados digitados pelo usuário no console
+		Scanner entrada = new Scanner (System.in);
+	
+		int n1, n2 =0;//Variáveis "n1" e "n2" (número 1 e número 2) criadas para guardar e representar os valores que o usuário utilizar para realizar operações
+		//matemáticas.
+		String op = "";//Variável que alojará a operação escolhida pelo usuário.
+		String escolha = "sim";//Variável que compõe o funcionamento da estrutura de repetição "while" 
+		int control = 0;
 
+		//Os códigos abaixo se assemelham aos desenvolvidos ao longo do projeto "CalculoDeArea" e já foram explicados. 
+		while(escolha.equals("Sim") || escolha.equals("sim") || escolha.equals("S") || escolha.equals("s") ) {
+			System.out.print("Digite a operação (+,-,/,*):");
+			op = entrada.nextLine();
+
+			//Definindo os valores
+			System.out.print("Digite o primeiro valor:");
+			n1 = Integer.parseInt(entrada.nextLine());
+
+			System.out.print("Digite o segundo valor: ");
+			n2 = Integer.parseInt(entrada.nextLine());
+			
+			if(op.equals("+")) {
+				System.out.println("O resultado da soma é: " + (n1+n2));
+				control++;
+			} else if(op.equals("-")) {
+				System.out.println("O resutlado da subtração é: " + (n1-n2) + " ou " + (n2-n1));
+				control++;
+			} else if(op.equals("*")) {
+				System.out.println("O resultado da multiplicação é: " + (n1*n2));
+				control++;
+			} else if(op.equals("/")) {
+				System.out.println("O resutlado da divisão é: " + (n1/n2) + " ou " + (n2/n1));
+				control++;
+			}else {
+				System.out.println("Operação invalida!");
+			}
+
+			System.out.print("Deseja efetuar mais cálculos?");
+			escolha = entrada.nextLine();
+			System.out.println("________________________________________");
+		}
+
+		}
+	}

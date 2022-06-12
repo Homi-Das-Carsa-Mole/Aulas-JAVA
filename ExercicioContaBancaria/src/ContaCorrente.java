7f7818573a3c410dbb8a9101bb1a2b8d02d2cbd2
Misael Gomes Macedo <102962890+zMSLz@users.noreply.github.com>
2022-06-12 08:54:26 -0300

    Comments

--- ExercicioContaBancaria/src/ContaCorrente.java
+++ ExercicioContaBancaria/src/ContaCorrente.java
@@ -1,10 +1,12 @@
 
-public class ContaCorrente extends ContaBancaria {
-	private double saldo = ContaBancaria.saldo;
-	
-	public double calcularSaldo() {
-		return saldo -= (saldo * 0.10); 
-	}
-	
 
+public class ContaCorrente extends ContaBancaria {//Classe que se estende de "ContaBancaria" e herda seus métodos
+	
+	@Override
+	//Ao calcular o saldo de uma conta do tipo "ContaIvestimento", susbtrai-se do saldo final 10% do saldo em conta, referente aos impostos que devem
+	//ser pagos.
+	public double calcularSaldo() {
+		return saldo -= (this.saldo * 0.1); 
+	}
 }
+

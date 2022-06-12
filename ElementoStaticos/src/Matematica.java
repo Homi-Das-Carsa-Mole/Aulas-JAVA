9c2de59f1056f9a8ba4a601c7cdd5c5a14de343b
Misael Gomes Macedo <102962890+zMSLz@users.noreply.github.com>
2022-06-11 23:10:22 -0300

    Comments

--- ElementoStaticos/src/Matematica.java
+++ ElementoStaticos/src/Matematica.java
@@ -1,5 +1,8 @@
 
 public class Matematica {
+	
+	//Métodos "somar" e "subtrair", ambos estáticos e que, para funcionarem, dependem apenas da inserção de dados do tipo "int" em seus parâmetros:
+	
 	public static int somar(int a, int b) {
 		return a + b;
 	}

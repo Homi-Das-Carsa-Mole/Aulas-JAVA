140c26f6743e19574ae8e3d289a636058571bb13
Misael Gomes Macedo <102962890+zMSLz@users.noreply.github.com>
2022-06-11 23:52:51 -0300

    Comments: super.[método da superclasse]

--- PolimorfismoBebidas/src/LeiteQuente.java
+++ PolimorfismoBebidas/src/LeiteQuente.java
@@ -7,9 +7,9 @@ public class LeiteQuente extends Bebida {
 
 	@Override
 	public void preparar() {
-		super.preparar();//O "super.preparar" está chamando as ações do método "preparar" da superclasse, de modo a executá-las juntamente com a linha de código abaixo.
-		//Chamamos o método "preparar" da superclasse "Bebida" porque nele está contida uma ação que é como que "universal" e válida para a preparação de qualquer
-		//bebida: um SYSO dizendo "Pegando o copo".
+		super.preparar();//O "super.preparar" está chamando o método "preparar" da superclasse, de modo a executá-lo juntamente com o Override deste método inserido
+		//nessa classe. Chamamos o método "preparar" da superclasse "Bebida" porque nele está contida uma ação que é como que "universal" e válida para a preparação
+		//de qualquer bebida: um SYSO dizendo "Pegando o copo".
 		System.out.println("Colocando o leite no copo");
 	}
 	

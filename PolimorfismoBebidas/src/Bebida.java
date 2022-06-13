411db207ee103dc0e52e62bade9d9cdcc48844cc
Misael Gomes Macedo <102962890+zMSLz@users.noreply.github.com>
2022-06-12 22:08:19 -0300

    Comments

--- PolimorfismoBebidas/src/Bebida.java
+++ PolimorfismoBebidas/src/Bebida.java
@@ -1,23 +1,21 @@
 
-public class Bebida {
+public class Bebida {//Classe da qual todas as outras classes de bebidas se estendem.
 	private String nome;
 	private boolean aquecer;
 	
-	/**
-	 * @param nome
-	 * @param aquecer
-	 */
-	
+	//Construtor de objetos da classe "Bebida", o qual exige que forneçamos para a bebida a ser construída um nome e se ela é quente ou não.
 	public Bebida(String nome, boolean aquecer) {
 		super();
 		this.nome = nome;
 		this.aquecer = aquecer;
 	}
 	
-	public void preparar() {
+	public void preparar() {//Método que é sobrescrito em cada subclasse de "Bebida", a fim de que estas possam dar mais detalhes sobre como é feito o seu preparo,
+	//mas que gera um SYSO necessário e comum entre o preparo de todas as bebidas.
 		System.out.println("Pegando o copo");
 	}
-
+	
+	//GETTERS AND SETTERS
 	public String getNome() {
 		return nome;
 	}

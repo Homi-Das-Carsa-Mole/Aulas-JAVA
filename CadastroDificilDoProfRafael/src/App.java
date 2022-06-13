41f4e05cabecd8dffbfe052a8e023fe5beecd83a
Misael Gomes Macedo <102962890+zMSLz@users.noreply.github.com>
2022-06-12 23:23:45 -0300

    Comments

--- CadastroDificilDoProfRafael/src/App.java
+++ CadastroDificilDoProfRafael/src/App.java
@@ -11,17 +11,21 @@ import java.util.Scanner;
 public class App {
 
 	public static void main(String[] args) throws FileNotFoundException, IOException {
-
+		
+		//Criação de listas que guardarão objetos do tipo "Pessoa", juntamente com seus dados (uma para escrita desses dados e outra para leitura destes):
 		List<Pessoa> listaDePessoas = new ArrayList<Pessoa>();
 		List<Pessoa> listaDePessoas2 = new ArrayList<Pessoa>();
 
 		Scanner entrada = new Scanner(System.in);
 
-		for (int x = 0; x < 1; x++) {
+		for (int x = 0; x < 4; x++) {//Laço de reptição "for", viabilizando o cadastro de 5 pessoas, neste caso.
+			//A cada loop, o objeto "pessoa" é instanciado novamente, porém, os dados personalizados de cada instância não são perdidos, já que, antes de
+			//finalizar o loop, adicionamos o objeto "pessoa" para "listaDePessoas", que atua como um simples banco de dados.
 
 			Pessoa pessoa = new Pessoa();
 			pessoa.setEndereco(new Endereco());
-
+			
+			//Usuário insere seus dados cadastrais:
 			System.out.println("Digite o seu nome: ");
 			pessoa.setNome(entrada.next());
 
@@ -56,14 +60,16 @@ public class App {
 			listaDePessoas.add(pessoa);
 
 		}
-
+		
+		//Enhanced-For que exibe os cadastros que "listaDePessoas" armazenou:
 		for (Pessoa p : listaDePessoas) {
 			System.out.println("Cadastros:");
 			System.out.println(p);
 			System.out.println("");
 
 		}
-
+		
+		//Escrevendo a lista em um arquivo "lista.txt"
 		try (BufferedWriter escrever = new BufferedWriter(new FileWriter("lista.txt"))) {
 
 			for (Pessoa p : listaDePessoas) {
@@ -71,7 +77,9 @@ public class App {
 			}
 
 		}
-
+		
+		//Lendo o arquivo de texto "lista.txt" e remontando, por assim dizer, os objetos da classe "Pessoa" e os adicionando à "listaDePessoas2", que agora irá conter
+		//todos os cadastros lidos em "lista.txt". 
 		try (BufferedReader reader = new BufferedReader(new FileReader("lista.txt"))) {
 			String linha;
 
@@ -83,7 +91,8 @@ public class App {
 			}
 
 		}
-
+		
+		//Mostrando na tela os cadastros lidos:
 		for (Pessoa pessoa : listaDePessoas2) {
 			System.out.println("Cadastros lidos do TXT:");
 			System.out.println(pessoa);

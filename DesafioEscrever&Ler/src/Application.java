8b3a8a5215f8a1884395a365259c2c3ddc2f58b3
Misael Gomes Macedo <102962890+zMSLz@users.noreply.github.com>
2022-06-12 22:52:22 -0300

    Comments

--- DesafioEscrever&Ler/src/Application.java
+++ DesafioEscrever&Ler/src/Application.java
@@ -13,17 +13,19 @@ public class Application {
 		System.out.println("Digite um texto qualquer");
 		
 		Scanner entrada = new Scanner (System.in); 
-		String textoEntrada = entrada.next();
+		String textoEntrada = entrada.next();//Atribuindo à variável "textoEntrada" um texto qualquer inserido pelo usuário no console. 
 		
 		try(BufferedWriter escrever = new BufferedWriter(new FileWriter("entrada.txt"))){
-			escrever.write(textoEntrada);
-			
+			escrever.write(textoEntrada);//Escrevendo no arquivo de texto "entrada.txt" o conteúdo armazenado em "textoEntrada". 
 		}
 
 		try(BufferedReader reader = new BufferedReader(new FileReader("entrada.txt"))) {
-			String line;
-			String textoSaida = "";
-			while ((line = reader.readLine()) != null) {
+			String line; //Variável "line", que irá comportar tudo que for lido nas linhas de texto do arquivo "entrada.txt".
+			String textoSaida = "";//Variável que receberá de "line"
+			while ((line = reader.readLine()) != null) {//Enquanto a variável "line" receber atribuições de conteúdos lidos no arquivo de texto - os quais são 
+			//detectados por "reader.readLine()" (leitor de linhas do arquivo de texto) - diferentes de "null", isto é, de vazio/nada, todas as linhas de conteúdo
+			//do arquivo "entrada.txt" que a variável "line" abrigar será somado e atribuído à "textoSaida", acrescido de uma quebra de linha, para que possa
+			//armazenar as diferentes linhas de conteúdo uma abaixo da outra.
 				textoSaida += line + "\n";
 			}
 			

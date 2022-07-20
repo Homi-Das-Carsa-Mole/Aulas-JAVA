07755d9b9eb38c795d1e006f7bbd0a2b5f8873ab
Aluno <Aluno@DSN1251210690>
2022-07-20 11:32:31 -0300

    Exercícios de revisão

---
+++ Exercicios/fundamentos/CalculadoraDeDescontos.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class CalculadoraDeDescontos {
+
+	public static void main(String[] args) {
+		
+		System.out.println("CALCULADORA DE DESCONTOS");
+		
+		Scanner entrada = new Scanner(System.in);
+		
+		System.out.println("Digite o valor do produto em reais:");
+		double valor = entrada.nextDouble();
+		
+		System.out.println("Digite o valor do desconto (Exemplo: 30 para 30 por cento.)");
+		int desc = entrada.nextInt();
+		
+		double vTotal = valor - (valor * desc / 100);
+		
+		System.out.println("O valor inserido para o produto foi R$" + valor + ".");
+		System.out.println("O desconto inserido foi de " + desc + "%.");
+		System.out.println("O valor final do produto é de R$" + vTotal + ".");
+		
+	}
+
+}

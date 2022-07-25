411e97d75a51b2e85193085fbf8a3d639f5485ca
Misael Gomes Macedo <102962890+zMSLz@users.noreply.github.com>
2022-07-25 16:41:07 -0300

    Exercícios 1 e 3 do PDF - JAVA

---
+++ ExerciciosPDF/src/MediaSalarial/Funcionarios.java
@@ -0,0 +1,60 @@
+package MediaSalarial;
+
+public class Funcionarios {
+	private String nome, cpf;
+	private Sexo sexo;
+	private Cargo cargo;
+	private double salario;
+	
+	public String getNome() {
+		return nome;
+	}
+	
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	
+	public String getCpf() {
+		return cpf;
+	}
+	
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+	
+	public Sexo getSexo() {
+		return sexo;
+	}
+	
+	public void setSexo(Sexo sexo) {
+		this.sexo = sexo;
+	}
+	
+	public Cargo getCargo() {
+		return cargo;
+	}
+	
+	public void setCargo(Cargo cargo) {
+		this.cargo = cargo;
+	}
+	
+	public void setSalario(Cargo cargo) {
+		this.salario = getCargo().getSalario();
+	}
+
+	public double getSalario() {
+		return salario;
+	}
+
+	@Override
+	public String toString() {
+		return "Nome: " + nome + "\nCPF: " + cpf + "\nSexo: " + sexo + "\nCargo: " + cargo + "\nSalário: R$"
+				+ salario + "\n-----------------------------------------------------------------";
+	}
+	
+	
+	
+	
+	
+	
+}

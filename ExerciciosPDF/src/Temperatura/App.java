9e52f68bf7b797c4221926cfdd2a652ff48ecbf5
Misael Gomes Macedo <102962890+zMSLz@users.noreply.github.com>
2022-07-28 01:38:17 -0300

    Retirando acentuações

--- ExerciciosPDF/src/Temperatura/App.java
+++ ExerciciosPDF/src/Temperatura/App.java
@@ -10,45 +10,45 @@ public class App {
 		Scanner entrada = new Scanner (System.in);
 		
 		System.out.println("Bem-vindo ao conversor de temperatura.");
-		System.out.println("Digite a temperatura em graus celsius que você deseja converter:");
+		System.out.println("Digite, em graus celcius, a temperatura que voce deseja converter:");
 		
 		double temperatura = entrada.nextDouble();
 		
 		int opcao = 0;
 		while (opcao < 1 || opcao > 5) {
-			System.out.println("Escolha para qual escala termométrica você deseja converter:");
-			System.out.println("1. Kelvin | 2. Réaumur | 3. Rankine | 4. Fahrenheit | 5. todas as escalas anteriores");
+			System.out.println("Escolha para qual escala termometrica voce deseja converter: ");
+			System.out.println("1. Kelvin | 2. Reaumur | 3. Rankine | 4. Fahrenheit | 5. todas as escalas anteriores");
 			opcao = entrada.nextInt();
 			
 			if (opcao == 1) {
-				System.out.println("A conversão de " + temperatura + " °C para Kelvin é igual a " + formulas.toKelvin(temperatura) + " K.");
+				System.out.println("A conversao de " + temperatura + " °C para Kelvin eh igual a " + formulas.toKelvin(temperatura) + " K.");
 			} else if (opcao == 2){
-				System.out.println("A conversão de " + temperatura + " °C para Réaumur é igual a " + formulas.toReaumur(temperatura) + " °Re.");
+				System.out.println("A conversao de " + temperatura + " °C para Reaumur eh igual a " + formulas.toReaumur(temperatura) + " °Re.");
 			} else if (opcao == 3) {
-				System.out.println("A conversão de " + temperatura + " °C para Rankine é igual a " + formulas.toRankine(temperatura) + " °Ra.");
+				System.out.println("A conversao de " + temperatura + " °C para Rankine eh igual a " + formulas.toRankine(temperatura) + " °R.");
 			} else if (opcao == 4) {
-				System.out.println("A conversão de " + temperatura + " °C para Fahrenheit é igual a " + formulas.toFahrenheit(temperatura) + " °F.");
+				System.out.println("A conversao de " + temperatura + " °C para Fahrenheit eh igual a " + formulas.toFahrenheit(temperatura) + " °F.");
 			} else if (opcao == 5) {
-				System.out.println("Conversão de " + temperatura + " °C para todas as escalas disponíveis:");
+				System.out.println("Conversao de " + temperatura + " °C para todas as escalas disponiveis:");
 				System.out.println("Kelvin: " + formulas.toKelvin(temperatura) + " K." );
-				System.out.println("Réaumur: " + formulas.toReaumur(temperatura) + " °Re.");
-				System.out.println("Rankine: " + formulas.toRankine(temperatura) + " °Ra.");
+				System.out.println("Reaumur: " + formulas.toReaumur(temperatura) + " °Re.");
+				System.out.println("Rankine: " + formulas.toRankine(temperatura) + " °R.");
 				System.out.println("Fahrenheit: " + formulas.toFahrenheit(temperatura) + " °F.");
 			} else {
-				System.out.println("Opção inválida. Tente novamente.");
+				System.out.println("COMANDO INEXISTENTE. Por favor, tente novamente.");
 				opcao = 0;
 			}
 			
 			int opcao2 = 0;
 			while (opcao2 != 1 && opcao2 != 2) {
-			System.out.println("Deseja continuar convertendo? 1. Sim | 2. Não");
+			System.out.println("Deseja continuar convertendo? 1. Sim | 2. Nao");
 			opcao2 = entrada.nextInt();
 			if (opcao2 == 1) {
 				opcao = 0;
 			} else if (opcao2 == 2) {
-				System.out.println("Fim do programa.");
+				System.out.println("Desejamos temperaturas agradaveis para voce e sua familia em todas as escalas possiveis. Agradecemos imensamente!");
 			} else {
-				System.out.println("Opção inválida. Tente novamente.");
+				System.out.println("COMANDO NEGADO - VALOR INEXISTENTE. Por favor, tente novamente.");
 			}
 			
 			}
